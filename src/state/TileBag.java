ab912fa5e7727882e7d42c15e2779e9b0e6c5183
Aaron Tetens <tetena573@gmail.com>
2021-05-21 01:02:26 -0400

    added TileBag and TileLocation default constructors

--- src/state/TileBag.java
+++ src/state/TileBag.java
@@ -1,11 +1,32 @@
 package state;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * This class represents the bag of tiles in the game and keeps track of the
- * tiles that enter and exit the bag as the game progresses.
+ * tiles that enter and exit the bag as the game progresses. This class also
+ * keeps track of which tiles are waiting in the lid of the game box to be put
+ * back in the bag once the bag runs out of tiles.
+ * 
+ * Tiles are stored as strings of length one: B = Blue, Y = Yellow, R = Red, K =
+ * Black, and W = White
  * 
  * @author Aaron Tetens
  */
 class TileBag {
 
+	private final Map<String, Integer> tilesInBag;
+	private final Map<String, Integer> tilesInLid;
+
+	TileBag() {
+		this.tilesInBag = new HashMap<>();
+		this.tilesInLid = new HashMap<>();
+
+		this.tilesInBag.put("B", 20);
+		this.tilesInBag.put("Y", 20);
+		this.tilesInBag.put("R", 20);
+		this.tilesInBag.put("K", 20);
+		this.tilesInBag.put("W", 20);
+	}
 }
--- src/state/TileLocation.java
+++ src/state/TileLocation.java
@@ -1,12 +1,23 @@
 package state;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * A TileLocation refers to a location in the game from which a player may take
  * or move tiles. The two subclasses of this class are Display and Table, which
  * are the two areas from which players take or move tiles throughout the game.
  * 
+ * Tiles are stored as strings of length one: B = Blue, Y = Yellow, R = Red, K =
+ * Black, and W = White
+ * 
  * @author Aaron Tetens
  */
 abstract class TileLocation {
 
+	private final Map<String, Integer> tiles;
+
+	TileLocation() {
+		this.tiles = new HashMap<>();
+	}
 }
