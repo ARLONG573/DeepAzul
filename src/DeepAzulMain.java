e4bc656b0528e8d787b8f36e827c652b61b87825
Aaron Tetens <tetena573@gmail.com>
2021-05-20 20:18:47 -0400

    initial commit

---
+++ src/DeepAzulMain.java
@@ -0,0 +1,12 @@
+/**
+ * This class contains the main method for the DeepAzul program.
+ * 
+ * @author Aaron Tetens
+ */
+public class DeepAzulMain {
+
+	public static void main(final String[] args) {
+
+	}
+
+}
